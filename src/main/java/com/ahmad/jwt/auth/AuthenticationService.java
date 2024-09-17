12f2d23843c0135cec3a10fb755242d66738b175
Ahmad Alhamoud <alhamoudahmad2000@gmail.com>
2024-09-17 13:40:37 +0300

    add exception handler & test the full auth flow

--- src/main/java/com/ahmad/jwt/auth/AuthenticationService.java
+++ src/main/java/com/ahmad/jwt/auth/AuthenticationService.java
@@ -125,6 +125,5 @@ public class AuthenticationService {
         userRepository.save(user);
         savedToken.setValidatedAt(LocalDateTime.now());
         tokenRepository.save(savedToken);
-
     }
 }
